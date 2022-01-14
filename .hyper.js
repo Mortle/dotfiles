4d3b9cc5912640de4b8847a2ce7880a525407511
Mortle <3vinogradof@gmail.com>
2022-01-14 18:23:06 +0300

    Added Hyper conf and .gitconfig

---
+++ .hyper.js
@@ -0,0 +1,10 @@
+module.exports = {
+  config: {
+    fontSize: 13,
+    fontFamily: 'Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+    shell: '/usr/bin/fish',
+    windowSize: [1000, 600],
+    scrollback: 10000,
+    padding: '2px 2px'
+  }
+};
