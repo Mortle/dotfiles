3745902e2d03a0c6aa3756a65b1b6a25a8a90257
Mortle <3vinogradof@gmail.com>
2022-01-14 18:22:49 +0300

    Added fish configuration

---
+++ fish/config.fish
@@ -0,0 +1,14 @@
+set fish_greeting "Broadcasting from square brawl..."
+
+set -x -g TERM   xterm-256color
+set -x -g LC_ALL en_GB.UTF-8
+set -x -g LANG   en_GB.UTF-8
+set -x -g EDITOR nano
+
+# Aliases
+alias g git
+alias b bundle
+
+# Bindings
+bind \cr  peco_search_cmd
+bind \cd  delete-char
